99a5a2d37b07e2d548626e14ff9eba650e3fdb29
Zayn Yusuf <yusue786@gmail.com>
2024-09-09 17:25:50 +0600

    change tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,36 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      container: {
+        center: true,
+        screens: {
+          lg: "1075px",
+          xl: "1075px",
+          "2xl": "1075px",
+        },
+      },
+      fontFamily: {
+        Assistant: ["Assistant", "sans-serif"],
+        OpenSans: ["Open Sans", "sans-serif"],
+        Roboto: ["Roboto", "sans-serif"],
+      },
+      colors: {
+        "custom-red": "#d7282f",
+        "custom-red2": "#b52227",
+        "custom-rose": "#f74356",
+        "custom-gray": "#eeeeee",
+        "custom-gray2": "#fcfcfc",
+        "custom-gray3": "#1b1f29",
+        "custom-gray4": "#333333",
+        "custom-gray5": "#414141",
+        "custom-gray5": "#414141",
+        "custom-gray6": "#414141",
       },
     },
   },
